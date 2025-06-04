28695ed5918ecf4f5342b5926b9cfdda82716f5e
Decpalm <declanpalmateeer@gmail.com>
2025-06-04 17:35:58 -0400

    Added drawing functionality

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const container = document.querySelector(".container");
+
 for (let i = 0; i < 16*16; i++){
     let div = document.createElement("div");
     div.classList.add("box");
     container.appendChild(div);
 }
+
+const boxs = document.querySelectorAll(".box")
+
+boxs.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+        box.classList.add("hover")
+    })
+});
