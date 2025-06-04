0a53764be0d50b2dd63b48087bea11326d676b76
Decpalm <declanpalmateeer@gmail.com>
2025-06-04 17:09:36 -0400

    Updated all files to have a 16x16 grid

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container");
+for (let i = 0; i < 16*16; i++){
+    let div = document.createElement("div");
+    div.classList.add("box");
+    container.appendChild(div);
+}
