d5433accbbbafb5ae5b607071accd42c9e4acd06
Decpalm <declanpalmateeer@gmail.com>
2025-06-04 21:50:03 -0400

    Updated script.js. Added functionality for changing grid size and reseting the grid

--- script.js
+++ script.js
@@ -1,15 +1,63 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".sub.container");
+const reset = document.querySelector(".reset");
+const rescale = document.querySelector(".grid");
+const text = document.querySelector(".text");
+let gridSize = 16;
+let boxs;
 
-for (let i = 0; i < 16*16; i++){
-    let div = document.createElement("div");
-    div.classList.add("box");
-    container.appendChild(div);
+function gridSetup(){
+    for (let i = 0; i < gridSize*gridSize; i++){
+        let div = document.createElement("div");
+        div.classList.add("box");
+        div.style.width = `calc(100% / ${gridSize})`;
+        div.style.height = `calc(100% / ${gridSize})`;
+        container.appendChild(div);
+    }
+
+    boxs = document.querySelectorAll(".box")
+    boxs.forEach((box) => {
+        box.addEventListener("mouseover", () => {
+            box.classList.add("hover")
+        })
+    });
+    text.textContent = gridSize + " x " + gridSize;
 }
 
-const boxs = document.querySelectorAll(".box")
+function clearGrid(){
+    container.innerHTML = '';
+}
 
-boxs.forEach((box) => {
-    box.addEventListener("mouseover", () => {
-        box.classList.add("hover")
-    })
-});
+reset.addEventListener("mouseover", () => {
+    reset.style.cssText = "background-color: red;"
+})
+
+reset.addEventListener("mouseout", () => {
+    reset.style.cssText = "background-color: lightcoral;"
+})
+
+reset.addEventListener("click", () => {
+    boxs.forEach((box) => {
+        box.classList.remove("hover");
+    });
+})
+
+rescale.addEventListener("mouseover", () => {
+    rescale.style.cssText = "background-color: green;"
+})
+
+rescale.addEventListener("mouseout", () => {
+    rescale.style.cssText = "background-color: lightgreen;"
+})
+
+rescale.addEventListener("click", () => {
+    let temp = parseInt(prompt("Enter a new grid size (1-100)"))
+    while(temp > 100){
+        alert("Error");
+        temp = parseInt(prompt("Enter a new grid size (1-100)"));
+    }
+    gridSize = temp;
+    clearGrid();
+    gridSetup();
+})
+
+window.onload = gridSetup();
